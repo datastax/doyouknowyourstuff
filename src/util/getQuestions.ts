de0cdb969898417f4be77a3b5ffd99b86116959c
Tejas Kumar <tejas@tejas.qa>
2024-08-27 14:42:27 +0200

    Adjust

--- src/util/getQuestions.ts
+++ src/util/getQuestions.ts
@@ -5,20 +5,15 @@ import { processUrl } from "./processUrl";
 dotenv.config();
 
 export const getQuestions = async (url: string) => {
-  // const cache = db.collection("question_cache");
-  // const cachedQuestions = await cache.findOne({ url });
-  // if (cachedQuestions) {
-  //   return cachedQuestions.questions;
-  // }
-
   const data = await fetch(process.env.LANGFLOW_URL!, {
     method: "post",
     headers: {
       "content-type": "application/json",
+      authorization: `Bearer ${process.env.LANGFLOW_API_KEY}`,
     },
     body: JSON.stringify({
       tweaks: {
-        "TextInput-AQrEg": {
+        "Text Input": {
           input_value: url,
         },
       },
@@ -26,11 +21,8 @@ export const getQuestions = async (url: string) => {
   })
     .then((res) => res.json())
     .then((d) => {
-      console.dir(d, { depth: Infinity });
       return JSON.parse(d.outputs[0].outputs[0].results.message.data.text);
     });
 
-  // await cache.insertOne({ url, questions: data.questions });
-  await db.collection(processUrl(url)).drop();
   return data.questions;
 };
